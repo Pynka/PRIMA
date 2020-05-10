b5576dfe535d41c396f59a3a50e4dea9d249ce69
Alissa Usta <39257952+Pynka@users.noreply.github.com>
2020-05-10 20:24:24 +0200

    gut

--- L02_SnakeStart/Main.ts
+++ L02_SnakeStart/Main.ts
@@ -1,32 +1,41 @@
-namespace L02_FirstFudge {
-    import ƒ = FudgeCore;
+namespace L02_SnakeStart {
+  import ƒ = FudgeCore;
 
-    window.addEventListener("load", hndLoad);
-    export let viewport: ƒ.Viewport;
+  window.addEventListener("load", hndLoad);
+  export let viewport: ƒ.Viewport;
 
-    function hndLoad(_event: Event): void {
-        const canvas: HTMLCanvasElement = document.querySelector("canvas");
-        ƒ.RenderManager.initialize();
-        ƒ.Debug.log(canvas);
+  function hndLoad(_event: Event): void {
+    const canvas: HTMLCanvasElement = document.querySelector("canvas");
+    ƒ.Debug.log(canvas);
 
-        let node: ƒ.Node = new ƒ.Node("Quad");
+    let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
+    let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
 
-        let mesh: ƒ.MeshQuad = new ƒ.MeshQuad();
-        let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
-        node.addComponent(cmpMesh);
-        
-        let mtrSolidWhite: ƒ.Material = new ƒ.Material("SolidWhite", ƒ.ShaderUniColor, new ƒ.CoatColored(ƒ.Color.CSS("WHITE")));
-        let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
-        node.addComponent(cmpMaterial);
+    let snake: ƒ.Node = new ƒ.Node("Snake");
 
-        let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
-        cmpCamera.pivot.translateZ(2);
-        cmpCamera.pivot.rotateY(180);
+    for (let i: number = 0; i < 4; i++) {
+      let node: ƒ.Node = new ƒ.Node("Quad");
 
-        viewport = new ƒ.Viewport();
-        viewport.initialize("Viewport", node, cmpCamera, canvas);
-        ƒ.Debug.log(viewport);
+      let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+      node.addComponent(cmpMesh);
+      cmpMesh.pivot.scale(ƒ.Vector3.ONE(0.8));
 
-        viewport.draw();
+      let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(mtrSolidWhite);
+      node.addComponent(cmpMaterial);
+
+      node.addComponent(new ƒ.ComponentTransform(ƒ.Matrix4x4.TRANSLATION(new ƒ.Vector3(-1 * i, 0, 0))));
+
+      snake.appendChild(node);
     }
+
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    cmpCamera.pivot.translateZ(10);
+    cmpCamera.pivot.rotateY(180);
+
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", snake, cmpCamera, canvas);
+    ƒ.Debug.log(viewport);
+
+    viewport.draw();
+  }
 }
